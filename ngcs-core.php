8784f918685a5b13b430c6358ca231470032a457
ngcs25 <n.gadoun@gmail.com>
2025-11-25 17:07:46 +0200

    Update plugin version from 2.01 to 3.00

--- ngcs-core.php
+++ ngcs-core.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: NGCS Core
  * Description: Main core engine for NGCS platform (Excel Messaging Engine, WA automation, n8n integration)
- * Version: 2.01
+ * Version: 3.00
  * Author: NGCS
  */
 
